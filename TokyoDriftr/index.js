838a013b26908250aceef6c8df15e2fbe1049fe8
Luke Harvey <Lukeharvey0226@gmail.com>
2020-07-07 08:41:24 -0700

    Minor Changes

--- TokyoDriftr/index.js
+++ TokyoDriftr/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser'); 
 const user = require('./models/userSchema.js');
 
